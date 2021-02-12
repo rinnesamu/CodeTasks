d906efd9439c64ea22790ea2690328b8666d598a
Samu <samu.rinne@metropolia.fi>
2021-02-12 18:50:26 +0200

    add simple solution to check that tests work

--- src/leastnumbersbetween/LeastNumbersBetween.java
+++ src/leastnumbersbetween/LeastNumbersBetween.java
@@ -4,7 +4,31 @@ import java.util.List;
 
 public class LeastNumbersBetween {
 
+  private final int ERROR_CODE = -1;
+
   public int getLeastNumbersBetween(List<Integer> list){
-    return 0;
+    int returnInt = 1111111;
+    if (list == null || list.size() == 0){
+      return -1;
+    }
+
+    if (list.size() == 1){
+      return 0;
+    }
+
+    for (int i = 0; i < list.size()-1; i++){
+      for (int j = i+1; j < list.size(); j++){
+        if (list.get(i) == list.get(j)){
+          return 0;
+        }
+        int numbersBetween = Math.abs(list.get(i) - list.get(j)) - 1;
+        if (numbersBetween < returnInt){
+          returnInt = numbersBetween;
+        }
+
+      }
+
+    }
+    return returnInt;
   }
 }
