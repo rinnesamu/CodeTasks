6ed988def36ea134369f94276f3e2d51c78cff0c
Samu <samu.rinne@metropolia.fi>
2021-02-12 18:10:55 +0200

    Add test stubs

---
+++ src/leastnumbersbetween/LeastNumbersBetween.java
@@ -0,0 +1,10 @@
+package leastnumbersbetween;
+
+import java.util.List;
+
+public class LeastNumbersBetween {
+
+  public int getLeastNumbersBetween(List<Integer> list){
+    return 0;
+  }
+}
---
+++ src/leastnumbersbetween/LeastNumbersBetweenTest.java
@@ -0,0 +1,51 @@
+package leastnumbersbetween;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeastNumbersBetweenTest {
+
+  LeastNumbersBetween least;
+  List<Integer> list = new ArrayList();
+
+  @BeforeAll
+  void beforeAll(){
+    least = new LeastNumbersBetween();
+  }
+
+  @Test
+  void testNoList(){
+  }
+
+  @Test
+  void tesWithOneItem(){
+  }
+
+  @Test
+  void testWithMultipleSameItems(){
+  }
+
+  @Test
+  void testSameIntervals(){
+  }
+
+  @Test
+  void testAscendingOrder(){
+  }
+
+  @Test
+  void testDescendingOrder(){
+  }
+
+  @Test
+  void testRandomOrder(){
+  }
+
+
+
+}
